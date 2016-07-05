8513a615f9d5822ab5aeffe1b32299d5f305a29a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-05 01:13:56 +0000

    done

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,9 @@
 module SongsHelper
-
+  def artist_select(artist_id, song)
+    if song.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name) 
+    else
+      hidden_field_tag "song[artist_id]", artist_id
+    end
+  end
 end
